9954e9337c7661448376eb5ec125559c3e8ec1b6
Oyedotun Oyesanmi <dotman14@users.noreply.github.com>
2016-10-16 18:36:10 -0400

    Create Display.cs

---
+++ Display.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace P3_oyedotunOyesanmi
+{
+    public static class Display
+    {
+        public static void GetMenu()
+        {
+            Console.WriteLine("------------ Menu Display ------------");
+            Console.WriteLine("Option 0) Clear and Load data ");
+            Console.WriteLine("Option 1) Add an item");
+            Console.WriteLine("Option 2) Modify Dataset");
+            Console.WriteLine("Option 3) Search Dataset");
+            Console.WriteLine("Option 4) Display Dataset Count");
+            Console.WriteLine("Option 5) Exit");
+            Console.WriteLine("------------------------------------ \n");
+        }
+    }
+}
