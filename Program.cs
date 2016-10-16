ec5b39a0c0c3c55a6eb535c91920ccc68b87238f
Oyedotun Oyesanmi <dotman14@users.noreply.github.com>
2016-10-16 18:36:57 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace P3_oyedotnOyesanmi
+namespace P3_oyedotunOyesanmi
 {
-    class Program
+    class MainClass
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            bool response = false;
+            int getChar;
+            while (response == false)
+            {
+                Display.GetMenu();
+
+                Console.Write("Select option: ");
+                getChar = Convert.ToInt16(Console.ReadLine());
+
+                switch (getChar)
+                {
+                    case 0:
+                        Console.Write("Option 0\n");
+                        break;
+                    case 1:
+                        Console.Write("Option 1\n\n");
+                        break;
+                    case 2:
+                        Console.Write("Option 2\n\n ");
+                        break;
+                    case 3:
+                        Console.Write("Option 3\n\n");
+                        break;
+                    case 4:
+                        Console.Write("Option 4\n\n");
+                        break;
+                    case 5:
+                        response = true;
+                        break;
+                    default:
+                        Console.Write("Wrong option\n\n");
+                        break;
+                }
+            }
         }
     }
 }
