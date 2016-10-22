17430a4df28f7af372c82e34ee86a7ac9f56a985
dotun <dotun@DESKTOP-8V1RGJC>
2016-10-22 16:07:25 -0400

    just me goofing around.

--- ElectionDataSet.cs
+++ ElectionDataSet.cs
@@ -16,10 +16,10 @@ namespace P3
             /* This constructor uses chaining.
              * Same could be achieved by:
              * 1. Creating a local ReadCsv object
-             * 2. Call Content Property on the object...That returns a list of string arrays 
+             * 2. Call CsvContent Property on the object...That returns a list of string arrays 
              * 3. Use LINQ to convert List<string[]> into List<ElectionData>
              */
-            Data = new ReadCsv().Content.Select(x => new ElectionData(x[0], CultureInfo.CurrentCulture.TextInfo.ToTitleCase(x[1].ToLower()), x[2], x[3], Convert.ToInt32(x[4]), Convert.ToInt32(x[5]), x[6], Convert.ToInt32(x[7]), x[8])).ToList();
+            Data = new ReadCsv().CsvContent.Select(x => new ElectionData(x[0], CultureInfo.CurrentCulture.TextInfo.ToTitleCase(x[1].ToLower()), x[2], x[3], Convert.ToInt32(x[4]), Convert.ToInt32(x[5]), x[6], Convert.ToInt32(x[7]), x[8])).ToList();
         }
 
         protected internal int Count => Data.Count;
--- ReadCSV.cs
+++ ReadCSV.cs
@@ -8,27 +8,24 @@ namespace P3
     class ReadCsv
     {
         //Raw election data from online resource.
-        private string _content;
+        private string content;
 
         //List of election data array.
-        private readonly List<string[]> _list = new List<string[]>();
+        private readonly List<string[]> list = new List<string[]>();
 
         /* Array of indexes corresponding to the specific election data we want to extract from CSV
          * Office -- 0, State -- 1, RaceDate -- 2, Area -- 4, TotalVotes -- 6, RepVotes -- 7
          * RepCandidate -- 8, DemVotes -- 10, DemCandidate -- 11
          */
-        private readonly int[] _indexWeNeed = { 0, 1, 2, 5, 6, 7, 8, 10, 11 };
+        private readonly int[] indexWeNeed = { 0, 1, 2, 5, 6, 7, 8, 10, 11 };
 
-        public List<string[]> Content
+        public List<string[]> CsvContent
         {
             get
             {
                 using (var url = new WebClient())
-                {
-                    _content =
-                        url.DownloadString(
-                            "https://docs.google.com/spreadsheets/d/1SlWCDS02UWqHRvSUJk3mFgcAStpaXLRBqi2ZIA58TFE/pub?output=csv");
-                }
+                    content = url.DownloadString("https://docs.google.com/spreadsheets/d/1SlWCDS02UWqHRvSUJk3mFgcAStpaXLRBqi2ZIA58TFE/pub?output=csv");
+               
 
                 /* The method above works just fine, but in case of network issues, download the CSV file from the above URL.
                  * Uncomment the code below and edit path to csv file in your file system, then comment the block of code above.
@@ -37,7 +34,7 @@ namespace P3
                 //_content = System.IO.File.ReadAllText(@"C:\Users\Phase3\Documents\Visual Studio 2015\Projects\P3\presidential2008.csv");
 
                 //Split raw election data into rows
-                var electionRow = _content?.Split('\n');
+                var electionRow = content?.Split('\n');
 
                 /* Foreach row, split by commas and insert into a separate array.
                  * For this to work as it should, make sure there are no commas within each data cell, except comma separating each data.
@@ -54,14 +51,11 @@ namespace P3
                     /* For each row, we get data for the specific index we want.
                      * Then we Add the string array into a List.
                      */
-                    var result = _indexWeNeed.Select(j => contentArr[j]).ToArray();
-                    _list.Add(result);
+                    var result = indexWeNeed.Select(j => contentArr[j]).ToArray();
+                    list.Add(result);
                 }
-                return _list;
+                return list;
             }
         }
     }
 }
-
-
-
