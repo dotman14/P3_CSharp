94f95033d4fb11e75ea2e3a289867072eb5089ce
raygi <r.alavo@gmail.com>
2016-11-16 18:56:14 -0500

    query

--- Linq.cs
+++ Linq.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace P3
 {
-    class Linq
+    class Linq : ElectionDataSet
     {
-       // public int 
+        public void numberDemocratic()
+        {
+            int year; 
+            Console.WriteLine("Input the year:");
+            year = Convert.ToInt32(Console.ReadLine());
+            var result = from n in Data
+                where n.Date.Year == year
+                group n by n.Date.Year
+                into demVote
+                select new
+                {
+                    totalDem = demVote.Sum(n => n.DemocratVote)
+                };
+
+            Console.WriteLine("In {0}, {1}",year, result);
+        }
     }
 }
